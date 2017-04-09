bd3a308fd0c3bc569d4507bfe32c27bf37bd363f
huanghy <545731930@qq.com>
2017-04-09 17:27:11 +0800

    SharedUtil工具类提交

--- app/src/main/java/com/example/huanghy/myapp/Main2Activity.java
+++ app/src/main/java/com/example/huanghy/myapp/Main2Activity.java
@@ -1,13 +1,14 @@
 package com.example.huanghy.myapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
